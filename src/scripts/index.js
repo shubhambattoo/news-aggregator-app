1298d11704e701d17181913e3031a68a37ecf3ae
shubhambattoo <shubham.battoo@hotmail.com>
2020-02-08 15:18:59 +0530

    added env file for keeping secret keys

---
+++ src/scripts/index.js
@@ -0,0 +1,2 @@
+console.log(process.env.APIKEY);
+console.log("js loaded");
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -3,6 +3,7 @@ const Webpack = require('webpack');
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -20,7 +21,8 @@ module.exports = merge(common, {
     new Webpack.optimize.ModuleConcatenationPlugin(),
     new MiniCssExtractPlugin({
       filename: 'bundle.css'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
